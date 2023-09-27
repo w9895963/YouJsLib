b7f1623c5a4fde1cf5b69805374b323de1e3329f
Puff <12085367@163.com>
2023-09-27 09:53:32 +0800

    -

--- dist/deps/ImportVueUiInjectDependencies.js
+++ dist/deps/ImportVueUiInjectDependencies.js
@@ -6,18 +6,19 @@
 
 export default async function () {
     var vueUi注入依赖库列表 = [
-        'https://unpkg.com/jquery.min.js',//jquery
-        'https://unpkg.com/vue@3/dist/vue.global.prod.js',//vue
-        'https://unpkg.com/element-plus/dist/index.css',//elementUI.css
-        'https://unpkg.com/element-plus/dist/index.full.min.js',//elementUI.js
+        'https://cdn.staticfile.org/jquery/3.7.1/jquery.min.js',//jquery
+        'https://cdn.staticfile.org/vue/3.3.4/vue.global.prod.min.js',//vue
+        'https://cdn.staticfile.org/element-plus/2.3.14/index.min.css',//elementUI.css
+        'https://cdn.staticfile.org/element-plus/2.3.14/index.full.min.js',//elementUI.js
     ];
+    
 
 
     var fn = await import("https://jsd.onmicrosoft.cn/gh/w9895963/YouJsLib@1.0.7/dist/Functions.js")
-    
+
     await fn.AddToHeadIfNotExist(vueUi注入依赖库列表);
 
 
-    window.Enumerable = (await import('https://cdnjs.cloudflare.com/ajax/libs/linq.js/4.0.1/linq.min.js')).default;//linq.js
+    window.Enumerable = (await import('https://cdn.staticfile.org/linq.js/4.0.1/linq.min.js')).default;//linq.js
 
 }
